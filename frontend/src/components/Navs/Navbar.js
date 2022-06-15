65c4b2e458921785a14f02f150d6a49e7629db13
Kamil Kwiatkowski <kaczy.kwiatkowski@gmail.com>
2022-06-15 17:55:27 +0200

    custom hook

--- frontend/src/components/Navs/Navbar.js
+++ frontend/src/components/Navs/Navbar.js
@@ -1,179 +1,279 @@
-import { Box, ButtonGroup, Stack } from "@mui/material"
+import {
+  Box,
+  ButtonGroup,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Stack,
+} from "@mui/material"
 import * as React from "react"
 import SearchBar from "./SearchBar"
 import { useDispatch, useSelector } from "react-redux"
 import { Link, useNavigate } from "react-router-dom"
 import { logoutAction } from "../../redux/slices/users/usersSlices"
 import { useTranslation } from "react-i18next"
-
+import KeyboardArrowDown from "@mui/icons-material/KeyboardArrowDown"
 import UserIcon from "./NavIcons/UserIcon"
-
+import useWindowDimensions from "../../hooks/WindowDimensionHook"
 import { ToggleSwitch } from "./ToggleSwitch"
 import MenuIcon from "@mui/icons-material/Menu"
 import SwitchLanguage from "./SwitchLanguage"
 function Navbar() {
+  const [open, setOpen] = React.useState(false)
+  const [open1, setOpen1] = React.useState(false)
+  const [expanded, setExpanded] = React.useState(false)
+  const { height, width } = useWindowDimensions()
   const navigate = useNavigate()
   const dispatch = useDispatch()
   const { userAuth } = useSelector((state) => state.users)
   const { t } = useTranslation()
 
   const collection = useSelector((state) => state?.collection.collectionList)
-
+React.useEffect(()=>{
+if(width > 990){
+  setExpanded(true)
+}
+},[width])
   return (
     <Box
       sx={{
         bgcolor: "background.default",
         color: "text.primary",
+        boxShadow: "rgba(117, 115, 115, 0.25) 0px 1px 12px;",
       }}
-      className="navbar navbar-expand-lg "
+      className="header__navigation"
     >
-      <ButtonGroup
-        className="navbar-toggler"
-        type="button"
-        data-mdb-toggle="collapse"
-        data-mdb-target="#navbarTogglerDemo01"
-        aria-controls="navbarTogglerDemo01"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <MenuIcon />
-      </ButtonGroup>
-      <Box className="collapse navbar-collapse" id="navbarTogglerDemo01">
-        <Stack minWidth="100%" className="navbar-nav">
-          <Box className="d-flex" style={{ flexBasis: "100%" }}>
+      <Box className="navbar__toggler">
+        <ListItemButton onClick={() => setExpanded(!expanded)}>
+          <MenuIcon sx={{ cursor: "pointer" }} />
+        </ListItemButton>
+      </Box>
+      
+      {expanded ? (
+        <Stack className="hide__navbar">
+          <Box style={{ display: "flex", flexBasis: "100%" }}>
             <Box className="iconNav" sx={{ alignSelf: "center" }}>
               <ToggleSwitch />
             </Box>
             <SwitchLanguage />
-            <Box class="navbar__logo">Funny Images</Box>
+            <Box className="navbar__logo">Funny Images</Box>
           </Box>
 
-          <Box className="d-flex wraplist">
+          <Box sx={{ display: "flex" }} className="wraplist">
             {userAuth?.isAdmin ? (
               <Box
-                className="nav-item dropdown"
-                sx={{ maxWidth: "max-content", ml: 1 }}
+                sx={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
               >
-                <Link
-                  to=""
-                  className="nav-link dropdown-toggle"
-                  id="navbarDropdownMenuspannk"
-                  role="button"
-                  data-mdb-toggle="dropdown"
-                  aria-expanded="false"
+                <ListItemButton
+                  sx={{
+                    width: "100%",
+                    height: "25px",
+                  }}
+                  onClick={() => setOpen(!open)}
                 >
-                  Admin
-                </Link>
-                <Box
-                  className="dropdown-menu"
-                  aria-labelledby="navbarDropdownMenuspannk"
-                >
-                  <Box className="dropdown-item">
-                    <Link to="/manage-users" className="nav-link">
-                      {t("Manage_Users")}
-                    </Link>
-                    <Link to="/admin-collection" className="nav-link">
-                      {t("Manage_users_collections")}
-                    </Link>
-                  </Box>
-                </Box>
+                  <ListItemText primary="Admin" />
+                  <KeyboardArrowDown
+                    sx={{
+                      opacity: 0,
+                      transform: open ? "rotate(-180deg)" : "rotate(0)",
+                      transition: "0.2s",
+                    }}
+                  />
+                </ListItemButton>
+                {open ? (
+                  <List
+                    sx={{
+                      backgroundColor: "#fff",
+                      color: "#000",
+                      border: "1px solid #000",
+                      display: "flex",
+                      flexDirection: "column",
+                      position: "absolute",
+                      top: "100%",
+                      cursor: "pointer",
+                      zIndex: "1",
+                    }}
+                  >
+                    <ListItem>
+                      <Link to="/manage-users" className="nav-link">
+                        {t("Manage_Users")}
+                      </Link>
+                    </ListItem>
+                    <ListItem>
+                      <Link to="/admin-collection" className="nav-link">
+                        {t("Manage_users_collections")}
+                      </Link>
+                    </ListItem>
+                  </List>
+                ) : null}
               </Box>
             ) : null}
             {userAuth ? (
               <Box sx={{ display: "flex", flexDirection: "row" }}>
                 <Box
-                  className="nav-item dropdown"
-                  sx={{ maxWidth: "max-content", ml: 1 }}
+                  sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
                 >
-                  <Link
-                    to=""
-                    className="nav-link dropdown-toggle"
-                    id="navbarDropdownMenuLink"
-                    role="button"
-                    data-mdb-toggle="dropdown"
-                    aria-expanded="false"
+                  <ListItemButton
+                    sx={{
+                      width: "100%",
+                      height: "25px",
+                    }}
+                    onClick={() => setOpen1(!open1)}
                   >
-                    {t("Collection")}
-                  </Link>
-                  <Box
-                    className="dropdown-menu"
-                    aria-labelledby="navbarDropdownMenuLink"
-                  >
-                    <Box className="dropdown-item">
-                      <Link to="/create-collection" className="nav-link">
-                        {t("Create_Collection")}
-                      </Link>
-                    </Box>
-                    <Box className="dropdown-item">
-                      <Link to="/view-collections" className="nav-link">
-                        {t("View_Collections")}
-                      </Link>
-                    </Box>
-                    <Box className="dropdown-item">
-                      <Link to="/my-collections" className="nav-link">
-                        {t("My_Collections")}
-                      </Link>
-                    </Box>
-                  </Box>
+                    <ListItemText primary={t("Collection")} />
+                    <KeyboardArrowDown
+                      sx={{
+                        opacity: 0,
+                        transform: open1 ? "rotate(-180deg)" : "rotate(0)",
+                        transition: "0.2s",
+                      }}
+                    />
+                  </ListItemButton>
+                  {open1 ? (
+                    <List
+                      sx={{
+                        backgroundColor: "#fff",
+                        color: "#000",
+                        border: "1px solid #000",
+                        display: "flex",
+                        flexDirection: "column",
+                        position: "absolute",
+                        top: "100%",
+                        cursor: "pointer",
+                        zIndex: "1",
+                      }}
+                    >
+                      <ListItem>
+                        <Link to="/create-collection">
+                          {t("Create_Collection")}
+                        </Link>
+                      </ListItem>
+                      <ListItem>
+                        <Link to="/view-collections">
+                          {t("View_Collections")}
+                        </Link>
+                      </ListItem>
+                      <ListItem>
+                        <Link to="/my-collections">{t("My_Collections")}</Link>
+                      </ListItem>
+                    </List>
+                  ) : null}
                 </Box>
                 <Box
-                  className="nav-item"
-                  sx={{ maxWidth: "max-content", ml: 1 }}
+                  sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
                 >
-                  <Link
-                    to=""
-                    className="nav-link"
-                    onClick={() =>
-                      dispatch(logoutAction()).then(() => {
-                        navigate("/")
-                      })
-                    }
+                  <ListItemButton
+                    sx={{
+                      width: "100%",
+                      height: "25px",
+                    }}
+                    onClick={() => dispatch(logoutAction())}
                   >
-                    {t("Logout")}
-                  </Link>
+                    <ListItemText primary={t("Logout")} />
+                  </ListItemButton>
+                </Box>
+                <Box
+                  sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  <ListItem
+                    sx={{
+                      width: "100%",
+                      height: "25px",
+                    }}
+                  >
+                    <ListItemText primary={<UserIcon />} />
+                  </ListItem>
                 </Box>
               </Box>
             ) : (
-              <Box sx={{ display: "flex", flexDirection: "row" }}>
-                <Box className="nav-item dropdown">
-                  <Link
-                    to=""
-                    className="nav-link dropdown-toggle"
-                    id="navbarDropdownMenuLink"
-                    role="button"
-                    data-mdb-toggle="dropdown"
-                    aria-expanded="false"
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <ListItemButton
+                  sx={{
+                    width: "100%",
+                    height: "25px",
+                  }}
+                  onClick={() => setOpen1(!open1)}
+                >
+                  <ListItemText primary={t("Collection")} />
+                  <KeyboardArrowDown
+                    sx={{
+                      opacity: 0,
+                      transform: open1 ? "rotate(-180deg)" : "rotate(0)",
+                      transition: "0.2s",
+                    }}
+                  />
+                </ListItemButton>
+                {open1 ? (
+                  <List
+                    sx={{
+                      backgroundColor: "#fff",
+                      color: "#000",
+                      border: "1px solid #000",
+                      display: "flex",
+                      flexDirection: "column",
+                      position: "absolute",
+                      top: "100%",
+                      cursor: "pointer",
+                      zIndex: "1",
+                    }}
                   >
-                    {t("Collection")}
-                  </Link>
-                  <Box
-                    className="dropdown-menu"
-                    aria-labelledby="navbarDropdownMenuLink"
-                  >
-                    <Box className="dropdown-item">
+                    <ListItem>
                       <Link to="/view-collections" className="nav-link">
                         {t("View_Collections")}
                       </Link>
-                    </Box>
-                  </Box>
-                </Box>
-                <Box className="nav-item" sx={{ minWidth: "fit-content" }}>
-                  <Link className="nav-link" to="/">
-                    {t("Login")}
-                  </Link>
+                    </ListItem>
+                  </List>
+                ) : null}
+                <Box
+                  sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  <ListItemButton
+                    sx={{
+                      width: "100%",
+                      height: "25px",
+                    }}
+                  >
+                    <Link to="/">
+                      <ListItemText sx={{color:"text.primary"}}primary={t("Login")}></ListItemText>
+                    </Link>
+                  </ListItemButton>
                 </Box>
               </Box>
             )}
-            <Box className="nav-link">
-              <UserIcon />
-            </Box>
 
             <Box sx={{ mr: 1, minWidth: "8rem" }}>
               <SearchBar data={collection} />
             </Box>
           </Box>
         </Stack>
-      </Box>
+      ) : null}
     </Box>
   )
 }
--- frontend/src/components/Navs/SwitchLanguage.js
+++ frontend/src/components/Navs/SwitchLanguage.js
@@ -1,9 +1,12 @@
-import { Box } from '@mui/material'
+import KeyboardArrowDown from '@mui/icons-material/KeyboardArrowDown'
+import { Box, List, ListItem, ListItemButton, ListItemText } from '@mui/material'
 import i18next from 'i18next'
 import React from 'react'
 import { useTranslation } from 'react-i18next'
+import { Link } from 'react-router-dom'
 
 function SwitchLanguage() {
+  const [open,setOpen] = React.useState(false)
     const {t} = useTranslation()
      const switchLanguage = () => {
        if (i18next.language === "en") {
@@ -13,43 +16,101 @@ function SwitchLanguage() {
        }
      }
   return (
-    <Box className="nav-item dropdown iconNav2" sx={{alignSelf:"center"}}>
-      <Box
-        className="nav-link dropdown-toggle text-reset"
-        to="#"
-        id="navbarDropdown"
-        role="button"
-        data-mdb-toggle="dropdown"
-        aria-expanded="false"
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <ListItemButton
+        sx={{
+          width: "100%",
+          height: "25px",
+          
+        }}
+        onClick={() => setOpen(!open)}
       >
-        {i18next.language === "en" ? (
-          <i className="flag-united-kingdom flag m-0"></i>
-        ) : (
-          <i className="flag-poland flag"></i>
-        )}
-      </Box>
-      <Box className="dropdown-menu " aria-labelledby="navbarDropdown">
-        <Box>
-          <p className="dropdown-item" onClick={() => switchLanguage()}>
-            <i className="flag-united-kingdom flag"></i>
-            {t("English")}
-            {i18next.language === "en" ? (
-              <i className="fa fa-check text-success ms-2"></i>
-            ) : null}
-          </p>
-        </Box>
+        <ListItemText
+        sx={{justifyContent:'center'}}
+          primary={
+            i18next.language === "en" ? (
+              <i className="flag-united-kingdom flag m-0"></i>
+            ) : (
+              <i className="flag-poland flag"></i>
+            )
+          }
+        />
 
-        <Box>
-          <p className="dropdown-item" onClick={() => switchLanguage()}>
-            <i className="flag-poland flag"></i>
-            {t("Polish")}
-            {i18next.language === "pl" ? (
-              <i className="fa fa-check text-success ms-2"></i>
-            ) : null}
-          </p>
-        </Box>
-      </Box>
+
+      </ListItemButton>
+      {open ? (
+        <List
+          sx={{
+            backgroundColor: "#fff",
+            color: "#000",
+            border: "1px solid #000",
+            display: "flex",
+            flexDirection: "column",
+            position: "absolute",
+            top: "100%",
+            cursor: "pointer",
+            zIndex: "1",
+          }}
+        >
+          <ListItem>
+            <Box className="dropdown-item" onClick={() => switchLanguage()}>
+              <i className="flag-united-kingdom flag"></i>
+              {t("English")}
+              {i18next.language === "en" ? (
+                <i className="fa fa-check text-success ms-2"></i>
+              ) : null}
+            </Box>
+          </ListItem>
+          <ListItem>
+            <Box className="dropdown-item" onClick={() => switchLanguage()}>
+              <i className="flag-poland flag"></i>
+              {t("Polish")}
+              {i18next.language === "pl" ? (
+                <i className="fa fa-check text-success ms-2"></i>
+              ) : null}
+            </Box>
+          </ListItem>
+        </List>
+      ) : null}
     </Box>
+
+    // <Box className="nav-item dropdown iconNav2" sx={{alignSelf:"center"}}>
+    //   <Box
+    //     className="nav-link dropdown-toggle text-reset"
+    //     to="#"
+    //     id="navbarDropdown"
+    //     role="button"
+    //     data-mdb-toggle="dropdown"
+    //     aria-expanded="false"
+    //   >
+    //     {i18next.language === "en" ? (
+    //       <i className="flag-united-kingdom flag m-0"></i>
+    //     ) : (
+    //       <i className="flag-poland flag"></i>
+    //     )}
+    //   </Box>
+    //   <Box className="dropdown-menu " aria-labelledby="navbarDropdown">
+    //     <Box>
+    //       <p className="dropdown-item" onClick={() => switchLanguage()}>
+    //         <i className="flag-united-kingdom flag"></i>
+    //         {t("English")}
+    //         {i18next.language === "en" ? (
+    //           <i className="fa fa-check text-success ms-2"></i>
+    //         ) : null}
+    //       </p>
+    //     </Box>
+
+    //     <Box>
+
+    //     </Box>
+    //   </Box>
+    // </Box>
   )
 }
 
